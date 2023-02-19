372a8993c3a8f6a8e25beb91e295be43752ce817
A7madelelamy <102883773+A7madelelamy@users.noreply.github.com>
2023-02-19 20:52:02 +0200

    Add files via upload

---
+++ Accounting for money companies (2).js
@@ -0,0 +1,22 @@
+const menu = document.getElementById("menu");
+const ex = document.getElementById("ex");
+const main = document.getElementById("main");
+
+menu.addEventListener("click",
+function () {
+  ex.style.display='inline';
+  menu.style.display='none';
+  main.style.display='block';
+  ex.style.color='white';
+
+console.log('kkfanfwaf')
+}
+);
+ex.addEventListener("click",
+function () {
+  menu.style.display='inline';
+  ex.style.display='none';
+  main.style.display='none';
+
+}
+);

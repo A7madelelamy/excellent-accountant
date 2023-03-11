b7496f167dfbbdbc715a384d1de73146f71f3adb
A7madelelamy <102883773+A7madelelamy@users.noreply.github.com>
2023-03-11 21:10:36 +0200

    Add files via upload

--- Accounting for money companies.js
+++ Accounting for money companies.js
@@ -1,6 +1,9 @@
 const menu = document.getElementById("menu");
 const ex = document.getElementById("ex");
 const main = document.getElementById("main");
+const dad = document.getElementById("dad");
+const xmark = document.getElementById("xmark");
+const darkmode = document.getElementById("darkmode");
 
 menu.addEventListener("click",
 function () {
@@ -20,3 +23,20 @@ function () {
 
 }
 );
+setTimeout(dadRun, 5000);
+
+function dadRun() {
+  dad.style.display="flex";
+  console.log("ia working");
+}
+xmark.addEventListener("click",function () {
+  dad.style.display='none';
+});
+function dark() {
+  const element = document.body;
+  dad.style.display='none';
+menu.classList.toggle("dark-mode-menu");
+  element.classList.toggle("dark-mode");
+  main.classList.toggle("dark-mode-main");
+}
+darkmode.addEventListener("click",dark);
